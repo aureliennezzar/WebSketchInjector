677c907a2750ab9dc7cbcac08ca13ce0e08d0739
subskill-aurelien <a.nezzar@subskill.com>
2022-02-24 12:05:41 +0100

    [0.1.1] -> Opacity range is now display after the sketch is loaded + minor bug fix

--- content-script.js
+++ content-script.js
@@ -1,15 +1,15 @@
-const _sheetId = "pseudoStyles";
-const createSketch = function (sketchBase64, opacity) {
+const _sheetClass = "web-sketch-injector-style";
+const _head = document.head || document.getElementsByTagName('head')[0];
+const createSketch = function (sketchBase64) {
 
     let file = new Image();
 
     file.onload = function () {
-        const _head = document.head || document.getElementsByTagName('head')[0];
-        const _sheet = document.getElementById(_sheetId) || document.createElement('style');
+        const _sheet = document.createElement('style');
         if (_sheet.length > 0) {
             _sheet.remove();
         }
-        _sheet.id = _sheetId;
+        _sheet.classList.add(_sheetClass);
         _sheet.innerHTML += `
 body{
     width:${file.width}px;
@@ -31,28 +31,56 @@ body:before{
     background-repeat: no-repeat;
     background-size: cover;
     background-image:url('${sketchBase64}');
-    opacity: ${opacity};
+    opacity: .5;
 }`
-
         _head.appendChild(_sheet);
         console.log('Web Sketch Injector $ Sketch resolution is : ' + file.width + 'px by ' + file.height + 'px')
     }
     file.src = sketchBase64;
 };
 const clearSketch = function () {
-    var _sheet = document.getElementById(_sheetId) || document.createElement('style');
-    if (_sheet) {
+    const _sheets = document.getElementsByClassName(_sheetClass);
+    if (_sheets.length > 0) {
+        Array.from(_sheets).forEach(sheet => {
+            sheet.remove();
+        })
+    }
+}
+
+const updateOpacity = function (opacity) {
+    const _sheet = document.createElement('style');
+    if (_sheet.length > 0) {
         _sheet.remove();
     }
+    _sheet.classList.add(_sheetClass);
+    _sheet.innerHTML += `
+    body:before{
+    opacity:${opacity} !important}`
+    _head.appendChild(_sheet);
 }
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         switch (request.func) {
             case 0:
-                createSketch(request.url, request.opacity)
+                try {
+                    createSketch(request.url)
+                } catch {
+                    alert('An error occured');
+                }
                 break;
             case 1:
+                // try {
                 clearSketch()
+                // } catch {
+                //     alert('An error occured');
+                // }
+                break;
+            case 2:
+                try {
+                    updateOpacity(request.opacity)
+                } catch {
+                    alert('An error occured');
+                }
                 break;
         }
     }
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//INIT
 function getBase64(file) {
     var reader = new FileReader();
     reader.readAsDataURL(file);
@@ -6,7 +7,7 @@ function getBase64(file) {
             chrome.tabs.sendMessage(tabs[0].id, {
                 func: 0,
                 url: reader.result,
-                opacity: slider.value / 100,
+                // opacity: slider.value / 100,
             });
         });
     };
@@ -15,34 +16,59 @@ function getBase64(file) {
     };
 }
 
-var slider = document.getElementById("opacity-range");
-var output = document.querySelector(".slidecontainer > p");
-output.style.left = ((((slider.value / 100) * ((slider.getBoundingClientRect().width - 7.5) - 7.5)) + 7.5) - (output.getBoundingClientRect().width * .15) - 7) + "px"
 
-output.innerHTML = slider.value +"%"; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function () {
-    output.style.left = (((((this.value - 0) / (100 - 0)) * ((slider.getBoundingClientRect().width - 7.5) - 7.5)) + 7.5) - (output.getBoundingClientRect().width * .15) -7) + "px"
-    output.innerHTML = this.value +"%";
-}
-
+//AFTER PLUGIN LOAD
 document.addEventListener('DOMContentLoaded', function () {
+
+    //CLEAR QUERY
     document.querySelector('.clean-btn').addEventListener('click', function () {
+        document.querySelector('.btn[type="submit"]').classList.remove('hidden')
+        document.querySelector('.slidecontainer').classList.add('hidden')
+        document.querySelector(".slidecontainer > p").innerHTML = '50%'
+        document.getElementById("opacity-range").value = 50;
         chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {
                 func: 1,
             });
         });
     })
+
+    //ON FILE INPUT CHANGE
     document.querySelector("#sketch-image").addEventListener('change', function (e) {
         document.querySelector('.upload-btn').style.border = 'none';
         document.querySelector('.image-msg').innerHTML = e.target.files[0].name;
     })
+
+    //ON SKETCH SUBMIT
     document.querySelector('form').addEventListener('submit', function (e) {
         e.preventDefault();
+
         const sketchInput = document.querySelector("#sketch-image");
         if (sketchInput.files.length > 0) {
+            //Display range slider
+            document.querySelector('.btn[type="submit"]').classList.add('hidden')
+            document.querySelector('.slidecontainer').classList.remove('hidden')
+
+            const slider = document.getElementById("opacity-range");
+            const output = document.querySelector(".slidecontainer > p");
+            output.style.left = ((((slider.value / 100) * ((slider.getBoundingClientRect().width - 7.5) - 7.5)) + 7.5) - (output.getBoundingClientRect().width * .15) - 7) + "px"
+
+            output.innerHTML = slider.value + "%"; // Display the default slider value
+            // Update the current slider value (each time you drag the slider handle)
+            slider.oninput = function () {
+                output.style.left = (((((this.value - 0) / (100 - 0)) * ((slider.getBoundingClientRect().width - 7.5) - 7.5)) + 7.5) - (output.getBoundingClientRect().width * .15) - 7) + "px"
+                output.innerHTML = this.value + "%";
+            }
+            //On range slider change
+            slider.addEventListener('change', function (e) {
+                chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        func: 2,
+                        opacity: e.target.value / 100,
+                    });
+                });
+            })
+
             getBase64(sketchInput.files[0])
         } else {
             document.querySelector('.upload-btn').style.boxShadow = '0 0 0 2px red';
